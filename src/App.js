f2d98e4f4681c67ab6fcea991a1934bd28d3b7c0
mrityunjay mishra <mrityunjay.83039@gmail.com>
2023-01-05 13:29:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let d = new Date().toLocaleTimeString();
+  const state = useState();
+  const [currentTime, setTime] = useState(d);
+
+
+ setInterval(() => {
+
+  d = new Date().toLocaleTimeString();
+  setTime(d);
+
+ },1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{display:'flex',alignItems:'center',height:'100vh',justifyContent:'center'}}>
+        <div className='clock__container'>
+          <h3 style={{textAlign:'center'}}>Digital Clock</h3>
+          <h1 style={{textAlign:'center'}}>{currentTime}</h1>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
